74241b4c9dbd9668146ff7c5c56fd0dd26cd4816
PYW-Mono <snake010807@naver.com>
2020-06-28 03:32:18 +0900

    issue #3 add calc and task files

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+	<body>
+		<form action="">
+			<input type="number" name="a">
+			+ <input type="number" name="b"><br>
+			<input type="submit">
+		</form>
+		<p>
+			%(c)s<br>
+			%(d)s<br>
+		</p>
+	</body>
+</html>
+"""
