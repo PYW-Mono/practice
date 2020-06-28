81e77c70edb4e081ed1ca35654ca7b83d56ef2a8
PYW-Mono <snake010807@naver.com>
2020-06-28 16:15:13 +0900

    issue #5 오류해결을 위해 first_number와 second_number의 기본값 0으로 설정

--- calc/plusmuli.py
+++ calc/plusmuli.py
@@ -3,9 +3,8 @@ from template import html
 
 def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
-	first_number = d.get('first_number', [''])[0]
-	second_number = d.get('second_number', [''])[0]
-	sum, mul = 0, 0
+	first_number = d.get('first_number', [0])[0]
+	second_number = d.get('second_number', [0])[0]
 	if '' not in [a, b]:
 		first_number, second_number = int(first_number), int(second_number)
 	        sum=first_number+second_number			
