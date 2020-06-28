af968b86d52c08f2d5c76e76dc1c5b194b3a3867
PYW-Mono <snake010807@naver.com>
2020-06-28 15:16:06 +0900

    issue #4 .conf 파일 원위치 완료

--- calc/serve.py
+++
@@ -1,8 +0,0 @@
-WSGIScriptAlias /calc /home/pyw20203069/gitpractice/practice/calc/plusmuli.py
-WSGIPythonPath /home/pyw20203069/gitpractice/practice/calc
-
-<Directory "/home/pyw20203069/gitpractice/practice/calc">
-	AllowOverride None
-	Order deny,allow
-	Require all granted
-</Directory>
