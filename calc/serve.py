a4b0569879dc2c88a899cd688cdc72d84d854ccb
PYW-Mono <snake010807@naver.com>
2020-06-28 15:09:26 +0900

    issue #4 잘못된 파일이동 확인됨 apache2의 .conf 파일을 calc로 옮겨옴 .conf 파일의 디렉토리 지정 코드 수정 다음 커밋에서 원 위치로 돌려놓을 것

--- calc/serve.py
+++ calc/serve.py
@@ -1,4 +1,4 @@
-WSGIScriptAlias /serve /home/pyw20203069/gitpractice/practice/calc/plusmuli.py
+WSGIScriptAlias /calc /home/pyw20203069/gitpractice/practice/calc/plusmuli.py
 WSGIPythonPath /home/pyw20203069/gitpractice/practice/calc
 
 <Directory "/home/pyw20203069/gitpractice/practice/calc">
